7968791b06db5dacc2e14869e384d9135ee5d22d
Roseanne Nguyen <rtnguye1@gmail.com>
2021-04-12 10:07:22 -0700

    set up header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Particles 
+      className="particles-canvas"
+      params={{
+        particles: {
+  
+          number: {
+            value: 30,
+            density:{
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "star",
+            stroke: {
+              width: 6,
+              color: "#1F6566"
+            }
+          }
+        }
+      }}/>
+    <Navbar />
+    <Header />
 
+    </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import '../Header/Header.css';
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
-        <div className="header-wraper">
-            
+        <div className="header-wrapper">
+            <canvas></canvas>
+           <div className="main-info">
+               <h1>Roseanne Nguyen's Portfolio</h1>
+               <Typed 
+               className="typed-text"
+               strings={["Web Design", "Web Development", "Node"]}
+               typeSpeed={40}
+               backSpeed={60}
+               loop
+                   />
+                   <a href="#" className="btn-main-offer">Connect with me</a>
+           </div>
         </div>
     )
 }
