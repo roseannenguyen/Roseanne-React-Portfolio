e5aa28c5145467c2ade495b2fb97ac2375daea87
Roseanne Nguyen <rtnguye1@gmail.com>
2021-04-12 09:11:11 -0700

    created navbar with toggle style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import logo from '../components/Images/logo192.png'
-import '../App.css';
+import logo from '../Images/logo192.png'
+import '../Navbar/Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 
 const Navbar = () => {
     return (
@@ -9,7 +12,7 @@ const Navbar = () => {
             <div className="container">
                 <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo.."></img></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={faBars} style={{color: '#fff'}}/>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
